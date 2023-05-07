87c9c1e7feb51051b63de3cc9d968aebcfee8a8c
Karam Isa <karam_isa@icloud.com>
2023-05-07 19:41:41 +0300

    minor changes

--- utils/data.js
+++ utils/data.js
@@ -8,11 +8,17 @@ const data = {
       isAdmin: true,
     },
     {
-      name: 'Mo',
-      email: 'mo@icloud,com',
+      name: 'Ahmad',
+      email: 'ahmad@admin.com',
+      password: bcrypt.hashSync('habibi'),
+      isAdmin: true,
+    },
+    {
+      name: 'John Doe',
+      email: 'user@gmail.com',
       password: bcrypt.hashSync('123456'),
       isAdmin: false,
-    }
+    },
   ],
     products: [
       {
@@ -54,43 +60,7 @@ const data = {
         numReviews: 3,
         countInStock: 20,
         description: 'A popular shirt',
-      },
-      {
-        name: 'Hoodie',
-        slug: 'hoodie',
-        category: 'Shirts',
-        image: '/images/pants1.jpg',
-        price: 90,
-        brand: 'Oliver',
-        rating: 2.9,
-        numReviews: 13,
-        countInStock: 20,
-        description: 'Smart looking pants',
-      },
-      {
-        name: 'Fit Pants',
-        slug: 'fit-pants',
-        category: 'Pants',
-        image: '/images/pants2.jpg',
-        price: 95,
-        brand: 'Zara',
-        rating: 3.5,
-        numReviews: 7,
-        countInStock: 20,
-        description: 'A popular pants',
-      },
-      {
-        name: 'Classic Pants',
-        slug: 'classic-pants',
-        category: 'Pants',
-        image: '/images/pants3.jpg',
-        price: 75,
-        brand: 'Casely',
-        rating: 2.4,
-        numReviews: 14,
-        countInStock: 20,
-        description: 'A popular pants',
-      },
+      }
     ],
   };
   
